61597c02c6922e1360105d7dc8f6acbc634cfbb9
MUScphillips <141957841+MUScphillips@users.noreply.github.com>
2023-08-23 13:09:26 +0000

    first commit

---
+++ src/ArithmeticOperators.java
@@ -0,0 +1,12 @@
+public class ArithmeticOperators
+{
+    public static void main(String[] args)
+    {
+        // Write three mathmatical equations that use +-* and / with numbers
+
+
+        // Write three mathmatical equations that use +-* and / with int and double variables
+
+        
+    }
+}
